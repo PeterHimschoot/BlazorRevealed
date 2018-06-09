eb1debfddff3f925550cdb4710319ac260e13028
Peter Himschoot <peter@u2u.be>
2018-06-09 12:16:15 +0200

    DataBinding to the menu

---
+++ PizzaPlace/PizzaPlace.Shared/Pizza.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzaPlace.Shared
+{
+  public enum Spiciness
+  {
+    None,
+    Spicy,
+    Hot
+  }
+
+  public class Pizza
+  {
+    public Pizza(int id, string name, decimal price, Spiciness spicyness)
+    {
+      this.Id = id;
+      this.Name = name ?? throw new ArgumentNullException(nameof(name), "A pizza needs a name!");
+      this.Price = price;
+      this.Spicyness = spicyness;
+    }
+
+    public int Id { get; }
+    public string Name { get; }
+    public decimal Price { get; }
+    public Spiciness Spicyness { get; }
+  }
+}
