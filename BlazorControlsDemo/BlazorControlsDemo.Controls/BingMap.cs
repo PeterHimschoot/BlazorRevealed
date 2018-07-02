6793f65c8c113ca70663f975aa3d1c53cbb7745d
Peter Himschoot <peter@u2u.be>
2018-07-02 15:39:15 +0200

    Create the map component
    It just needs to display in Blazor Client

---
+++ BlazorControlsDemo/BlazorControlsDemo.Controls/BingMap.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace BlazorControlsDemo.Controls
+{
+  public class BingMap
+  {
+    public static string Load(string id, string key)
+    {
+      return RegisteredFunction.Invoke<string>("BlazorControlsDemo.Controls.getMap", id, key);
+    }
+  }
+}
---
+++ BlazorControlsDemo/BlazorControlsDemo.Controls/content/getMap.js
@@ -0,0 +1,31 @@
+﻿// This file is to show how a library package may provide JavaScript interop features
+// wrapped in a .NET API
+
+var loadJS = function (url, implementationCode, location) {
+  //url is URL of external file, implementationCode is the code
+  //to be called from the file, location is the location to 
+  //insert the <script> element
+
+  var scriptTag = document.createElement('script');
+  scriptTag.src = url;
+
+  scriptTag.onload = implementationCode;
+  // scriptTag.onreadystatechange = implementationCode;
+
+  location.appendChild(scriptTag);
+};
+
+var yourCodeToBeCalled = function () {
+  //your code goes here
+  console.log('### Script loaded ###');
+  Blazor.registerFunction('BlazorControlsDemo.Controls.getMap', function (id, key) {
+    console.log('### Called getMap###')
+    var map = new Microsoft.Maps.Map('#' + id, {
+      credentials: key
+    });
+    return "OK";
+  });
+}
+
+loadJS('http://www.bing.com/api/maps/mapcontrol', yourCodeToBeCalled, document.body);
+
